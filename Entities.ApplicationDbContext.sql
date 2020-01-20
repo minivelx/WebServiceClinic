4a225ee5de44a82ea18d815dd719a0c91fb2360b
minivelx <minivelx@gmail.com>
2020-01-20 13:05:09 -0500

    readme and SQL scripts

---
+++ Entities.ApplicationDbContext.sql
@@ -0,0 +1,260 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [MedicalAppointmentType] (
+        [Id] int NOT NULL IDENTITY,
+        [Description] nvarchar(40) NOT NULL,
+        CONSTRAINT [PK_MedicalAppointmentType] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [Roles] (
+        [Id] nvarchar(450) NOT NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [Users] (
+        [Id] nvarchar(450) NOT NULL,
+        [UserName] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [Email] nvarchar(256) NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        [Name] nvarchar(100) NULL,
+        [PersonalID] nvarchar(20) NULL,
+        [Active] bit NOT NULL,
+        CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [MedicalAppointment] (
+        [Id] int NOT NULL IDENTITY,
+        [IdMedicalAppointmentType] int NOT NULL,
+        [Date] datetime2 NOT NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        [Active] bit NOT NULL,
+        CONSTRAINT [PK_MedicalAppointment] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_MedicalAppointment_MedicalAppointmentType_IdMedicalAppointmentType] FOREIGN KEY ([IdMedicalAppointmentType]) REFERENCES [MedicalAppointmentType] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [RoleId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [ProviderKey] nvarchar(450) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] nvarchar(450) NOT NULL,
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [Name] nvarchar(450) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+        CONSTRAINT [FK_AspNetUserTokens_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE TABLE [RolesUsers] (
+        [UserId] nvarchar(450) NOT NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_RolesUsers] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_RolesUsers_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_RolesUsers_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description') AND [object_id] = OBJECT_ID(N'[MedicalAppointmentType]'))
+        SET IDENTITY_INSERT [MedicalAppointmentType] ON;
+    INSERT INTO [MedicalAppointmentType] ([Id], [Description])
+    VALUES (1, N'Medicina General'),
+    (2, N'Odontología'),
+    (3, N'Pediatría'),
+    (4, N'Neurología');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description') AND [object_id] = OBJECT_ID(N'[MedicalAppointmentType]'))
+        SET IDENTITY_INSERT [MedicalAppointmentType] OFF;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [IX_MedicalAppointment_IdMedicalAppointmentType] ON [MedicalAppointment] ([IdMedicalAppointmentType]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE UNIQUE INDEX [RoleNameIndex] ON [Roles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [IX_RolesUsers_RoleId] ON [RolesUsers] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [Users] ([NormalizedEmail]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [Users] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200118225908_InitModelBuild')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200118225908_InitModelBuild', N'2.2.4-servicing-10062');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200120125208_ForeignKeyUserMedicalAppointment')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[MedicalAppointment]') AND [c].[name] = N'UserId');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [MedicalAppointment] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [MedicalAppointment] ALTER COLUMN [UserId] nvarchar(450) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200120125208_ForeignKeyUserMedicalAppointment')
+BEGIN
+    CREATE INDEX [IX_MedicalAppointment_UserId] ON [MedicalAppointment] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200120125208_ForeignKeyUserMedicalAppointment')
+BEGIN
+    ALTER TABLE [MedicalAppointment] ADD CONSTRAINT [FK_MedicalAppointment_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE NO ACTION;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200120125208_ForeignKeyUserMedicalAppointment')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200120125208_ForeignKeyUserMedicalAppointment', N'2.2.4-servicing-10062');
+END;
+
+GO
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WebServiceClinic
+
+- La estructura de la base de datos y datos iniciales pueden ser restaurados mediante el archivo 'Entities.ApplicationDbContext'
+- La conexión a la base de datos se debe configurar dentro del archivo 'appsettings.json' del WebApi
+- Se puede configurar el puerto de las peticiones realizadas a la WebApi dentro del archivo 'main-es5.[version].js' dentro de la publicación del ClientApp
+
+Nota: Las publicaciones WebApi y ClientApp son independientes.
